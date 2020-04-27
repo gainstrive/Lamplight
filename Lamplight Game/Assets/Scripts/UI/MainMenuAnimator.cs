2756db051c1a16bb3fb7b8c2a1430d927e3e34e9
gainstrive <gainstrive@gmail.com>
2020-04-27 10:03:45 -0500

    Added vfx and UI fn.
    Implemented an alpha-overlay video for main menu. Figured out audio event triggering via Event Systems.

--- Lamplight Game/Assets/Scripts/UI/MainMenuAnimator.cs
+++ Lamplight Game/Assets/Scripts/UI/MainMenuAnimator.cs
@@ -6,13 +6,21 @@ public class MainMenuAnimator : MonoBehaviour
 {
     public GameObject menuAnimator;
     public AudioManager audioManager;
+    public GameObject playGameMenu;
     // Start is called before the first frame update
     public void MainMenuOut()
     {
         menuAnimator = GameObject.Find("Main_Menu");
         menuAnimator.GetComponent<Animator>().Play("Main_Menu_Out", 0);
         audioManager.Play("CardFlipStart");
-
+        StartCoroutine(PlayMenuIn());
         
+    }
+
+    private IEnumerator PlayMenuIn()
+    {
+        yield return new WaitForSeconds(1.5f);
+        playGameMenu.SetActive(true);
+
     }
 }
--- Lamplight Game/Assets/Scripts/UI/PromoButton.cs
+++ Lamplight Game/Assets/Scripts/UI/PromoButton.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PromoButton : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip hoverSFX;
+    public AudioClip clickSFX;
 
     public void OpenURL() {
         Application.OpenURL("http://google.com/");
         Debug.Log("Working");
+    }
+
+    public void playHoverSFX()
+    {
+        Debug.Log(hoverSFX);
+        audioSource.PlayOneShot(hoverSFX);
     }
 }
