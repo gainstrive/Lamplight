ae42965541d741a86d3806e5abf86c178e35578b
gainstrive <gainstrive@gmail.com>
2020-04-27 11:47:14 -0500

    Fixed missing object ref.

--- Lamplight Game/Assets/Scripts/UI/MainMenuAnimator.cs
+++ Lamplight Game/Assets/Scripts/UI/MainMenuAnimator.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class MainMenuAnimator : MonoBehaviour
 {
     public GameObject menuAnimator;
-    public AudioManager audioManager;
     public GameObject playGameMenu;
     // Start is called before the first frame update
     public void MainMenuOut()
     {
         menuAnimator = GameObject.Find("Main_Menu");
         menuAnimator.GetComponent<Animator>().Play("Main_Menu_Out", 0);
-        audioManager.Play("CardFlipStart");
         StartCoroutine(PlayMenuIn());
         
     }
