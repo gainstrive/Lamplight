e90393a1e9b0639975f503f979ab084bceb08af4
gainstrive <gainstrive@gmail.com>
2020-04-27 05:46:29 -0500

    Finished first screen of the main menu.
    Added Post Processing effects and tweaked aesthetics.

---
+++ Lamplight Game/Assets/Scripts/UI/MainMenuAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAnimator : MonoBehaviour
+{
+    public GameObject menuAnimator;
+    public AudioManager audioManager;
+    // Start is called before the first frame update
+    public void MainMenuOut()
+    {
+        menuAnimator = GameObject.Find("Main_Menu");
+        menuAnimator.GetComponent<Animator>().Play("Main_Menu_Out", 0);
+        audioManager.Play("CardFlipStart");
+
+        
+    }
+}
---
+++ Lamplight Game/Assets/Scripts/UI/PlayButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject menuAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void MainMenuOut()
+    {
+        menuAnimator = GameObject.Find("Main_Menu");
+        menuAnimator.GetComponent<Animator>().Play("Main_Menu_Out", 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
